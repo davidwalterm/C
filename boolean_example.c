07a447eb8b3d6cd423212d4942bfe4335f16da3d
LAPTOP <david.walter.marinescu@gmail.com>
2024-08-08 19:26:08 +0300

    Added sizeof and boolean examples

---
+++ boolean_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int myAge = 25;
+    int votingAge = 18;
+
+    printf("%d", myAge >= votingAge); // Returns 1 (true), meaning 25 years olds are allowed to vote!
+
+    getch();
+
+    return 0;
+}
