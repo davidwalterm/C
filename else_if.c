8588a67d3096c6e46ad690dcaa712cbde38878a5
LAPTOP <david.walter.marinescu@gmail.com>
2024-08-08 19:40:18 +0300

    If Else Examples

---
+++ else_if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int time = 22;
+    if (time < 10) {
+        printf("Good morning!");
+    } else if (time < 20){
+        printf("Good day!");
+    } else {
+        printf("Good evening!");
+    }
+
+    getch();
+
+    return 0;
+}
---
+++ short_if_else.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int time = 20;
+    (time < 18) ? printf("Good day!") : printf("Good evening!");
+
+    getch();
+
+    return 0;
+}
